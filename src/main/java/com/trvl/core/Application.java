e2665088c577303e5af757b403cfa6ccf187819e
TsimpDim <dimitristsimpitas@gmail.com>
2021-04-07 11:49:48 +0300

    Fix CORS and fix login functionality

--- src/main/java/com/trvl/core/Application.java
+++ src/main/java/com/trvl/core/Application.java
@@ -4,7 +4,10 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan
@@ -14,4 +17,13 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/login").allowedOrigins("http://localhost:4200");
+			}
+		};
+	}
 }
--- src/main/java/com/trvl/core/controller/AuthController.java
+++ src/main/java/com/trvl/core/controller/AuthController.java
@@ -2,16 +2,20 @@ package com.trvl.core.controller;
 
 import com.trvl.core.dto.LogInRequestDTO;
 import com.trvl.core.dto.SignUpRequestDTO;
+import com.trvl.core.model.Token;
 import com.trvl.core.service.AuthService;
 import com.trvl.core.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 
 @Controller
 public class AuthController {
@@ -30,11 +34,17 @@ public class AuthController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @PostMapping("login")
-    public ResponseEntity<String> login(@Valid @RequestBody LogInRequestDTO loginRequestDTO) {
+    @PostMapping(value = "login", produces = "application/json")
+    @ResponseBody
+    public ResponseEntity<HashMap> login(@Valid @RequestBody LogInRequestDTO loginRequestDTO) {
         String token = authService.loginUser(loginRequestDTO);
+        HashMap<String, String> respData = new HashMap<>();
+        respData.put("token", token);
+        respData.put("username", loginRequestDTO.getUsername());
+
+
         return ResponseEntity.status(HttpStatus.OK)
                 .header("bearer", token)
-                .body(token);
+                .body(respData);
     }
 }
